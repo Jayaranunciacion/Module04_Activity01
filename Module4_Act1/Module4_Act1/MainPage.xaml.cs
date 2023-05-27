5db457274ef0f9175b61c2e6e24d638f05782673
LAPTOP-LAPLOFV5\acer <anunciacion.jay-ar@auf.efu.ph>
2023-05-27 19:36:33 +0800

    first commit

---
+++ Module4_Act1/Module4_Act1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Module4_Act1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module4_Act1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Open_One(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new One());
+        }
+        private async void Open_Two(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Two());
+        }
+    }
+}
